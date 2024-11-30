873c085e0d019181a57411f6e9c355e52b6af69e
little2 <agile@little2.net>
2024-12-01 05:11:21 +0800

    update

--- go3.py
+++ go3.py
@@ -78,9 +78,9 @@ try:
     #max_process_time 設為 600 秒，即 10 分鐘
     max_process_time = 60*27  # 25分钟
     max_media_count = 55  # 10个媒体文件
-    max_count_per_chat = 12  # 每个对话的最大消息数
-    # max_break_time = 90  # 休息时间
-    max_break_time = 60  # 休息时间
+    max_count_per_chat = 11  # 每个对话的最大消息数
+    max_break_time = 90  # 休息时间
+    
 
     # 创建 LYClass 实例
 
