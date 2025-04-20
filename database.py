7b9f28d2aaeaace5e706e4ab4b5cf4594e20de0a
little2 <agile@little2.net>
2025-04-20 22:53:07 +0800

    update

--- database.py
+++ database.py
@@ -9,12 +9,12 @@ if not os.getenv('GITHUB_ACTIONS'):
 
 
 db_config = {
-    'db_name': os.getenv('DB_NAME'),
-    'db_user': os.getenv('DB_USER'),
-    'db_password': os.getenv('DB_PASSWORD'),
-    'db_host': os.getenv('DB_HOST'),
+    'db_name': os.getenv('MYSQL_DB_NAME'),
+    'db_user': os.getenv('MYSQL_DB_USER'),
+    'db_password': os.getenv('MYSQL_DB_PASSWORD'),
+    'db_host': os.getenv('MYSQL_DB_HOST'),
     'db_sslmode': os.getenv('DB_SSLMODE','require'),
-    'db_port': int(os.getenv('DB_PORT',3306)),
+    'db_port': int(os.getenv('MYSQL_DB_PORT',3306)),
 }
 
 # 数据库配置
