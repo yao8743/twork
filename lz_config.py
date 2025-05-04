efa18d13fdee2fa0da7c785a780445e7291b9809
little2 <agile@little2.net>
2025-05-04 21:25:28 +0800

    update

--- lz_config.py
+++ lz_config.py
@@ -1,7 +1,16 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
 load_dotenv(dotenv_path='.lzbot.env')
 API_TOKEN = os.getenv("BOT_TOKEN")
 POSTGRES_DSN = os.getenv("POSTGRES_DSN")
+BOT_MODE = os.getenv("BOT_MODE", "polling").lower()
+
+WEBHOOK_HOST = os.getenv("WEBHOOK_HOST")
+WEBHOOK_PATH = os.getenv("WEBHOOK_PATH")
+WEBAPP_HOST = os.getenv("WEBAPP_HOST")
+WEBAPP_PORT = os.getenv("WEBAPP_PORT")
+
+
+
 
--- lz_main.py
+++ lz_main.py
@@ -3,11 +3,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from lz_config import API_TOKEN
+from lz_config import API_TOKEN,BOT_MODE,WEBHOOK_PATH,WEBAPP_HOST,WEBAPP_PORT,WEBHOOK_HOST
 from lz_db import db
 from handlers import lz_search_highlighted
 
-
+async def on_startup(bot: Bot):
+    await bot.set_webhook(f"{WEBHOOK_HOST}{WEBHOOK_PATH}")
 
 async def main():
 
@@ -19,16 +20,23 @@ async def main():
 
    
     # dp = Dispatcher()
-   
-
     dp = Dispatcher()
-    
-
-    
     dp.include_router(lz_search_highlighted.router)
     await db.connect()
    
-    await dp.start_polling(bot, polling_timeout=10.0)
+
+    if BOT_MODE == "webhook":
+        await dp.start_webhook(
+            bot,
+            webhook_path=WEBHOOK_PATH,
+            on_startup=on_startup,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT
+        )
+    else:
+        await dp.start_polling(bot, polling_timeout=10.0)
+
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
