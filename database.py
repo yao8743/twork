59635470dcc95e1ba4a85496b5f2fb30d3a891ce
little2 <agile@little2.net>
2025-05-22 08:47:50 +0800

    update

--- database.py
+++ database.py
@@ -39,7 +39,7 @@ else:
         'db_password': db_config.get('db_password', os.getenv('MYSQL_DB_PASSWORD')),
         'db_host': db_config.get('db_host', os.getenv('MYSQL_DB_HOST')),
         'db_sslmode': db_config.get('db_sslmode', os.getenv('MYSQL_DB_SSLMODE','require')),
-        'db_port': db_config.get('db_port', int(os.getenv('MYSQL_DB_PORT', 3306))),
+        'db_port': int(db_config.get('db_port', int(os.getenv('MYSQL_DB_PORT', 3306)))),
     }
 
 # 数据库配置
