87ea6e607510a6c77cca094b54470ec00240bd8e
little2 <agile@little2.net>
2025-05-22 08:44:49 +0800

    update

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from peewee import MySQLDatabase, OperationalError
 import re
 import os
+import json
 
 # # 检查是否在本地开发环境中运行
 # if not os.getenv('GITHUB_ACTIONS'):
@@ -21,13 +22,24 @@ if match:
         'db_port': int(match.group(4)),
     }
 else:
+    db_config = {}
+    # 嘗試載入 JSON 並合併參數
+    try:
+        setting_json = json.loads(os.getenv('CONFIGURATION', ''))
+        if isinstance(setting_json, dict):
+            db_config.update(setting_json)  # 將 JSON 鍵值對合併到 config 中
+    except Exception as e:
+        print(f"⚠️ 無法解析 CONFIGURATION：{e}")
+
+# //CONFIGURATION = '{"db_name":"telebot", "db_user":"telebot", "db_password":"GB]RcWbK9EQOxcdv", "db_host":"little2net.i234.me", "db_sslmode":"require", "db_port":58736}'
+
     db_config = {
-        'db_name': os.getenv('MYSQL_DB_NAME'),
-        'db_user': os.getenv('MYSQL_DB_USER'),
-        'db_password': os.getenv('MYSQL_DB_PASSWORD'),
-        'db_host': os.getenv('MYSQL_DB_HOST'),
-        'db_sslmode': os.getenv('DB_SSLMODE','require'),
-        'db_port': int(os.getenv('MYSQL_DB_PORT',3306)),
+        'db_name': db_config.get('db_name', os.getenv('MYSQL_DB_NAME')),
+        'db_user': db_config.get('db_user', os.getenv('MYSQL_DB_USER')),
+        'db_password': db_config.get('db_password', os.getenv('MYSQL_DB_PASSWORD')),
+        'db_host': db_config.get('db_host', os.getenv('MYSQL_DB_HOST')),
+        'db_sslmode': db_config.get('db_sslmode', os.getenv('MYSQL_DB_SSLMODE','require')),
+        'db_port': db_config.get('db_port', int(os.getenv('MYSQL_DB_PORT', 3306))),
     }
 
 # 数据库配置
