42efd7554076bf7bd59af6b9383ce122cb03409b
little2 <agile@little2.net>
2025-06-04 07:20:18 +0800

    update

--- reset.py
+++ reset.py
@@ -6,7 +6,7 @@ import os
 # Check if running in a local development environment
 if not os.getenv('GITHUB_ACTIONS'):
     from dotenv import load_dotenv
-    load_dotenv(dotenv_path='.24530779.env')
+    load_dotenv(dotenv_path='.28817994.env')
     
 
 
